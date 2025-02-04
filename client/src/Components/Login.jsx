3b89c9fbb139275be90d18959fc0cd82271057fb
Sara-git-218 <sarah74218@gmail.com>
2025-02-04 14:04:14 +0200

    aaa

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,13 +1,14 @@
-import React, { useRef } from 'react'; 
+import React, { useRef,useState } from 'react'; 
 import { Divider } from 'primereact/divider';
 import { InputText } from 'primereact/inputtext';
 import { Button } from 'primereact/button';
 import axios from 'axios'
+import Register from './Users/Register';
 
 export default function Login() {
     const userNameRef=useRef("")
     const passwordRef=useRef("")
-
+    const [visible, setVisible] = useState(false);
     const login=async()=>
         {
             try {
@@ -44,8 +45,11 @@ export default function Login() {
                         <b>או</b>
                     </Divider>
                 </div>
+                
                 <div className="w-full md:w-5 flex align-items-center justify-content-center py-5">
-                    <Button label="הרשמה" icon="pi pi-user-plus" severity="success" className="w-10rem"></Button>
+                    {/* <Button label="הרשמה" icon="pi pi-user-plus" severity="success" className="w-10rem"></Button> */}
+                    <Button label="הרשמה" severity="secondary" icon="pi pi-pencil" onClick={() => setVisible(true)} style={{ marginLeft: '0.5em'}} />
+                  {visible&&<Register  setVisible={setVisible} visible={visible}  />}
                 </div>
             </div>
         </div>
