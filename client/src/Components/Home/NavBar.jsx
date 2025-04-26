9265c28254f30d001520b37f2aa448847af6feae
Sara-git-218 <sarah74218@gmail.com>
2025-04-27 01:48:32 +0300

    continu......

--- client/src/Components/Home/NavBar.jsx
+++ client/src/Components/Home/NavBar.jsx
@@ -20,7 +20,6 @@ export default function NavBar() {
     const [visible2, setVisible2] = useState(false);
     const navigate = useNavigate();
     const dispatch = useDispatch();
-    // const isLoggedIn = useSelector((state) => state.IsLogIn.isLoggedIn);
     const token = useSelector(state => state.Token.tokenstr);
     const user = useSelector(state => state.User.user)
     const isAdmin = user?.roles === "Admin";
@@ -58,6 +57,7 @@ export default function NavBar() {
 
         await dispatch(setToken(""));
         await dispatch(setUser(null));
+        navigate('/')
     };
     const items =isAdmin
     ? [
@@ -160,31 +160,16 @@ export default function NavBar() {
         },
 
 
-        // {
-        //     label:visible2?<></>:<Button label="הרשמה" severity="secondary" onClick={() =>navigate('/Register')} dir='ltr' style={{ marginLeft: '1em'}} />//() => setVisible(true)
-        // }
-        // },
-        // {
-        //     label:visible2?<></>:<Button label="כניסה" severity="secondary" onClick={() =>navigate('/Login',{state:{setVisible2}})} dir='ltr' style={{ marginLeft: '1em'}} />
-        // },
-        // {
-        //     label:!visible2?<></>:<Button label="יציאה" severity="secondary" onClick={() =>logOut()}  dir='ltr' style={{ marginLeft: '1em'}} />
-        // }
-
     ];
     useEffect(() => {
         console.log("Updated token:", token,user);
     }, [token,user]);
 
-    // const logOut=()=>{
-    //     dispatch(setToken(""))
-    // localStorage.setItem("token","")}
+  
 
     return (
         <div className="card" >
             <Menubar model={items} className="NavBarcss" />
-            {/* {visible&&<Register  setVisible={setVisible} visible={visible}  />} */}
-            {/* {visible2&&<Login  setVisible={setVisible2} visible={visible2}  />} */}
         </div>
     )
 }
